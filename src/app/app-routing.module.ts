73310cefe82bccb0d13693f0ae7ed039eb588bec
santosh <santosh.ece06@gmail.com>
2018-10-19 16:18:52 +0530

    comp2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { Comp1Component } from './comp1/comp1.component';
 const routes: Routes = [
   {path: 'comp1', component: Comp1Component},
   {path: 'comp2', component: Comp2Component},
-  {path: '', component: Comp1Component},
+  {path: '', component: Comp2Component},
 ];
 
 @NgModule({
