55b0a81f6370612ccc67928f67e4203f4c7e492a
santosh <santosh.ece06@gmail.com>
2018-10-19 15:15:43 +0530

    Angular Routing and Material.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [Comp2Component, Comp1Component]
 })
 export class AppRoutingModule { }
