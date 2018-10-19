50daac2d72f896f0f4e98050fc2c8f5999e24d63
santosh <santosh.ece06@gmail.com>
2018-10-19 15:34:58 +0530

    update.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [Comp2Component, Comp1Component]
 })
 export class AppRoutingModule { }
 
