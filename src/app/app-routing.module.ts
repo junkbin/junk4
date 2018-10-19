cbba3fc824fd337764668e0cbedbaa0dd91341f0
santosh <santosh.ece06@gmail.com>
2018-10-19 18:52:54 +0530

    comp 5 added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,21 @@ import { Comp2Component } from './comp2/comp2.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp3Component } from './comp3/comp3.component';
 import { Comp4Component } from './comp4/comp4.component';
+import { Comp5Component } from './comp5/comp5.component';
 
 const routes: Routes = [
   {path: 'comp1', component: Comp1Component},
   {path: 'comp2', component: Comp2Component},
   {path: 'comp3', component: Comp3Component},
   {path: 'comp4', component: Comp4Component},
-  {path: '', component: Comp4Component},
+  {path: 'comp5', component: Comp5Component},
+  {path: '', component: Comp5Component},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [],
+  declarations: [Comp5Component],
 })
 export class AppRoutingModule { }
 
@@ -24,5 +26,6 @@ export const RoutingComponents = [
     Comp1Component,
     Comp2Component,
     Comp3Component,
-    Comp4Component
+    Comp4Component,
+    Comp5Component
 ];
